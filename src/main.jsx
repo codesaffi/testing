3bd83dafe4f72908ad57fd4a0c9be7ef88092ee6
codesaffi <saffi8946@gmail.com>
2024-10-08 18:21:15 +0500

    feat: routing

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,29 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Home from './pages/Home.jsx'
+import Contact from './pages/Contact.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: "/testing/",
+    element: <App />,
+    children: [
+      {
+        path: "/testing/",
+        element: <Home />,
+      },
+      {
+        path: "/testing/contact",
+        element: <Contact />,
+      },
+    ],
+  },
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+   <RouterProvider router={router} />
+  </StrictMode>
+);
